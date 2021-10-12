716e36971e24d588eac03864b7c7ab0362bd6de0
Thomas Lohan <johndoe@example.com>
2021-10-12 19:27:38 -0400

    updated commentform and commentlist

--- client/src/components/shared/Comments.js
+++ client/src/components/shared/Comments.js
@@ -1,57 +1,57 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-const Comments = ({ userId }) => {
-  const [locations, setComments] = useState([])
+// const Comments = ({ userId }) => {
+//   const [locations, setComments] = useState([])
 
-  useEffect( () => {
-    axios.get(`/api/medications/${medicationId}/comments`)
-      .then( res => {
-        setComments(res.data)
-      })
-      .catch( err => console.log(err))
-  }, [])
+//   useEffect( () => {
+//     axios.get(`/api/medications/${medicationId}/comments`)
+//       .then( res => {
+//         setComments(res.data)
+//       })
+//       .catch( err => console.log(err))
+//   }, [])
 
-  const addLocation = (location) => {
-    axios.post(`/api/medications/${medicationId}/comments`, { comment })
-      .then(res => {
-        setComments([...comments, res.data])
-      })
-      .catch( err => console.log(err))
-  }
+//   const addLocation = (location) => {
+//     axios.post(`/api/medications/${medicationId}/comments`, { comment })
+//       .then(res => {
+//         setComments([...comments, res.data])
+//       })
+//       .catch( err => console.log(err))
+//   }
 
-  const updateComment = (id, comment) => {
-    axios.put(`/api/medication/${medicationId}/comments/${id}`, { comment })
-      .then( res => {
-        let updatedComments = comments.map( c => {
-          if (c.id === id) {
-            return res.data
-          }
-          return c
-        })
-        setComments(updatedComments)
-      })
-      .catch( err => console.log(err))
-  }
+//   const updateComment = (id, comment) => {
+//     axios.put(`/api/medication/${medicationId}/comments/${id}`, { comment })
+//       .then( res => {
+//         let updatedComments = comments.map( c => {
+//           if (c.id === id) {
+//             return res.data
+//           }
+//           return c
+//         })
+//         setComments(updatedComments)
+//       })
+//       .catch( err => console.log(err))
+//   }
 
-  const deleteComment = (id) => {
-    axios.delete(`/api/medications/${medicationId}/comments/${id}`)
-      .then( res => {
-        setComments( comments.filter( c => c.id !== id))
-      })
-      .catch( err => console.log(err))
-  }
+//   const deleteComment = (id) => {
+//     axios.delete(`/api/medications/${medicationId}/comments/${id}`)
+//       .then( res => {
+//         setComments( comments.filter( c => c.id !== id))
+//       })
+//       .catch( err => console.log(err))
+//   }
 
-  return (
-    <>
- <CommentForm addComment={addComment} />
-      <CommentList 
-        comments={comments} 
-        deleteComment={deleteComment}
-        updateComment={updateComment}
-      />
-    </>
-  )
-}
+//   return (
+//     <>
+//  <CommentForm addComment={addComment} />
+//       <CommentList 
+//         comments={comments} 
+//         deleteComment={deleteComment}
+//         updateComment={updateComment}
+//       />
+//     </>
+//   )
+// }
 
-export default Comments;
+// export default Comments;
