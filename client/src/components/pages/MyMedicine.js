9d9728a865eff256fd38cf246ff4e33da1c2ff3d
“Meg <meg5ed@virginia.edu>
2021-10-21 21:32:38 -0600

    changed favicon

--- client/src/components/pages/MyMedicine.js
+++ client/src/components/pages/MyMedicine.js
@@ -2,8 +2,11 @@ import MedicationForm from '../medications/MedicationForm';
 import AllMyMedications from '../medications/AllMyMedications';
 import { Button, Segment } from 'semantic-ui-react';
 import ShowMedication from '../medications/ShowMedication';
+import { useState } from 'react';
 
-const MyMedicine = ({ MedicationForm}) => {
+const MyMedicine = ({ MedicationForm }) => {
+  const [on, setOn] = useState(false)
+   // class componen into a presentational component, and then use hooks to use features
   return(
   <>
   <Segment basic>
@@ -15,9 +18,7 @@ const MyMedicine = ({ MedicationForm}) => {
     <i class="plus icon"></i>
     </Button>
 
-    {/* <Segment>
-     {/* <MedicationForm /> */}
-     {/* </Segment> */} 
+      {/* <MedicationForm /> */}
   </Segment>
 
   </>
